11c9e627c28f37f3868e909d6a5e1c1e0b1c908b
mouravocal <diego.moura3@gmail.com>
2024-03-09 22:25:07 -0300

    feat: :sparkles: format Date

--- App.tsx
+++ App.tsx
@@ -1,14 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const [holydays, setHolydays] = useState([])
+  const [loading, setLoading] = useState(true)
+  const currentYear = new Date().getFullYear()
+  const url = `https://brasilapi.com.br/api/feriados/v1/${currentYear}`
+
+  const request = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setHolydays(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    request()
+  }, [])
+
+  if (loading)
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Feriados Nacionais</Text>
+      {holydays.map((holyday) => (
+        <View key={holyday.date}>
+          <Text>{holyday.name}</Text>
+          <Text>
+            {new Date(holyday.date).toLocaleDateString('pt-BR', {
+              dateStyle: 'full',
+            })}
+          </Text>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +51,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
